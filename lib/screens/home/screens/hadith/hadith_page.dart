9848c55afe5781c3e3d7f01a976632c4dbf0c3c8
Mustafa <mm0118250@gmail.com>
2025-07-15 00:25:50 +0300

    hadith files added and finished the hadith feature

--- lib/screens/home/screens/hadith/hadith_page.dart
+++ lib/screens/home/screens/hadith/hadith_page.dart
@@ -1,7 +1,9 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:islamy_app/screens/home/screens/hadith/widgets/hadith_item_widget.dart';
 
+import '../../../../models/hadith_model.dart';
 import '../../widgets/custom_header.dart';
 
 class HadithPage extends StatelessWidget {
@@ -33,7 +35,7 @@ class HadithPage extends StatelessWidget {
 class HadithCarousel extends StatefulWidget {
   const HadithCarousel({super.key});
 
-// final HadithModel hadithModel;
+  // final HadithModel hadithModel;
   @override
   State<HadithCarousel> createState() => _HadithCarouselState();
 }
@@ -44,22 +46,24 @@ class _HadithCarouselState extends State<HadithCarousel> {
 
   @override
   Widget build(BuildContext context) {
+    if (hadithList.isEmpty) {
+      loadHadithData();
+    }
     return Column(
       children: [
         CarouselSlider(
-
-          items: [
-            HadithItemWidget(),
-            HadithItemWidget(),
-            HadithItemWidget(),
-          ],
+          items: hadithList.map((e) {
+            return HadithItemWidget(hadithModel: e);
+          }).toList(),
           options: CarouselOptions(
             height: 618,
             autoPlay: false,
+            reverse: true,
             enlargeCenterPage: true,
             viewportFraction: .8,
             animateToClosest: true,
-
+            // scrollDirection: Axis.horizontal,
+            initialPage: 0,
             onPageChanged: (index, reason) {
               setState(() {
                 _current = index;
@@ -70,5 +74,24 @@ class _HadithCarouselState extends State<HadithCarousel> {
         const SizedBox(height: 20),
       ],
     );
+  }
+
+  List<HadithModel> hadithList = [];
+
+  Future<void> loadHadithData() async {
+    String content = await rootBundle.loadString(
+        'assets/files/combined_hadiths_separated.txt');
+    List<String> allHadith = content.split('#');
+
+    for (var element in allHadith) {
+      String singleHadith = element.trim();
+      int indexOfTitle = singleHadith.indexOf('\n');
+      String hadithTitle = singleHadith.substring(0, indexOfTitle);
+      String hadith = singleHadith.substring(indexOfTitle + 1);
+
+      setState(() {
+        hadithList.add(HadithModel(title: hadithTitle, content: hadith));
+      });
+    }
   }
 }
--- lib/screens/home/screens/hadith/widgets/hadith_item_widget.dart
+++ lib/screens/home/screens/hadith/widgets/hadith_item_widget.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 
-class HadithItemWidget extends StatelessWidget {
-  const HadithItemWidget({super.key});
+import '../../../../../models/hadith_model.dart';
 
+class HadithItemWidget extends StatelessWidget {
+  const HadithItemWidget({super.key, required this.hadithModel});
+
+  final HadithModel hadithModel;
   @override
   Widget build(BuildContext context) {
     return Container(
-      // width: double.infinity,
-      // height: 618,
       decoration: BoxDecoration(
         color: Color(0xffE2BE7F),
         borderRadius: BorderRadius.circular(20),
         image: DecorationImage(
-          opacity: .1,
+          opacity: .25,
           image: AssetImage('assets/images/hadith_card_background.png'),
           fit: BoxFit.contain,
         ),
@@ -44,27 +45,32 @@ class HadithItemWidget extends StatelessWidget {
             left: 20,
             right: 20,
             top: 40,
-            child: Column(
-              children: [
-                Text(
-                  'الحد يث الأول',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 24,
-                    fontWeight: FontWeight.w700,
+            child: SizedBox(
+              width: double.infinity,
+              height: 500,
+              child: ListView(
+                children: [
+                  Text(
+                    hadithModel.title,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                ),
-                SizedBox(height: 30),
-                Text(
-                  'hadiths[index]',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 20,
+                  SizedBox(height: 30),
+                  Text(
+                    hadithModel.content,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           Positioned(
